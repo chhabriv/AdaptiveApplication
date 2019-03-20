8bf9b4cc20d04597b2d04326e2ea0cdf3e3969ad
Ashwin Kumar <kumara1@tcd.ie>
2019-03-20 01:45:01 +0000

    Add files via upload

---
+++ POC/static/js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+	$('.slider').slider({full_width: true});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
