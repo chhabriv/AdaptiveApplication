0556552c0187f584b302ac6b251a722e4a191a04
Nicky <bonellon@tcd.ie>
2019-03-31 13:17:58 +0100

    fixed validation.py

--- POC/RoutingValidation/validation.py
+++ POC/RoutingValidation/validation.py
@@ -24,12 +24,6 @@ destination -> evening attraction.
 
 '''
 
-
-
-
-
-
-
 #def validateRoute(listofAttra):
 apiKey = 'AIzaSyB2NHLaqVDF0uSmuNBMXI3DVsUanzdRD7Q'
 url = 'https://maps.googleapis.com/maps/api/directions/json'
@@ -45,6 +39,15 @@ class Attraction:
     LatLng = 0
     Duration = 0
 
+    def __init__(self, Category, Name, OpeningHours, ClosingHours, Latitude, Longitude, LatLng, Duration):
+        self.Category = Category
+        self.Name = Name
+        self.OpeningHours = OpeningHours
+        self.ClosingHours = ClosingHours
+        self.Latitude = Latitude
+        self.Longitude = Longitude
+        self.LatLng = LatLng
+        self.Duration = Duration
 
 totalCostTime = 0;
 
@@ -65,7 +68,7 @@ def getTotalCostTime(data, attractions):
             totalTravelTime += round(currentTravelTime, 2)
 
             currentAttraction = attractions[i]
-            currentAttractionTime = int(currentAttraction['Duration'])
+            currentAttractionTime = int(currentAttraction.Duration)
             totalAttractionTime += currentAttractionTime
 
             #print(totalTravelTime)
@@ -82,29 +85,24 @@ def getTotalCostTime(data, attractions):
 
 attractions = []
 with open('objects.csv', 'r') as objectsCsv:
-    reader = csv.reader(objectsCsv)
+    reader = csv.reader(objectsCsv, delimiter=',')
     for row in reader:
-        row = row[0].split('\t')
-        attractions.append({
-            'Category': row[1],
-            'Name': row[2],
-            'OpeningHours': int(row[3]),
-            'ClosingHours': int(row[4]),
-            'Latitude': row[5],
-            'Longitude': row[6],
-            'Duration': row[7],
-            'LatLng': row[5] + "," + row[6]
-        })
+        #row = row[0].split('\t')
+
+        #def __init__(self, Category, Name, OpeningHours, ClosingHours, Latitude, Longitude, LatLng, Duration):
+
+        a = Attraction(row[1], row[2], row[3], row[4], row[5], row[6], row[5] + "," + row[6], row[7])
+        attractions.append(a)
 
 i = 0
 while (1):
 
     if (i < len(attractions) - 2):
         params = {
-            'origin': attractions[i]['LatLng'],
-            'destination': attractions[i + 1]['LatLng'],
+            'origin': attractions[i].LatLng,
+            'destination': attractions[i + 1].LatLng,
             'key': apiKey,
-            'waypoints': attractions[i + 2]['LatLng'],
+            'waypoints': attractions[i + 2].LatLng,
             'optimizeWaypoints': 'true'
         }
         r = requests.get(url, params=params)
@@ -129,33 +127,32 @@ while (1):
 print(totalCostTime)
 
 def getLatestOpeningHour(attractions):
-    max = 0
+    max = 800
     for attr in attractions:
-        tmp = attr['OpeningHours']
-        if (max < tmp):
-            max = tmp
+        openingHour = int(attr.OpeningHours)
+        if (max < openingHour):
+            max = openingHour
     return max
 
 
-def getEarlistClosingHour(attractions):
+def getEarliestClosingHour(attractions):
     min = 2359
     for attr in attractions:
-        tmp = attr['ClosingHours']
-        if (min > tmp):
-            min = tmp
+        closingHour = int(attr.ClosingHours)
+        if (min > closingHour):
+            min = closingHour
     return min
 
 print(getLatestOpeningHour(attractions))
-print(getEarlistClosingHour(attractions))
+print(getEarliestClosingHour(attractions))
 
 def validation(totalCostTime, attractions):
     hour = round(totalCostTime/60)
     min = round((totalCostTime%60))*0.6
     tmp = hour*100 + min
-    if (getLatestOpeningHour(attractions) + tmp <= getEarlistClosingHour(attractions)):
+    if (getLatestOpeningHour(attractions) + tmp <= getEarliestClosingHour(attractions)):
         return "The Trip is Valid"
     else:
         return "Not enough time to finish the trip"
-
 
 print(validation(totalCostTime, attractions))
