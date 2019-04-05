882b57c01a94a214a598b811db8c365e0f3e84e1
zihan <huangzi@tcd.ie>
2019-04-05 19:14:21 +0100

    add signup and login to the map page

--- code/ui/app.js
+++ code/ui/app.js
@@ -23,6 +23,9 @@ var esri_WorldImagery = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{
 var latestData;
 var currentRoute = 0
 
+var startMarker;
+var destMarker;
+var prevPolyline = []
 document.getElementById ("startBtn").addEventListener ("click", test);
 
 
@@ -54,16 +57,15 @@ document.getElementById ("Mary").addEventListener ("click", function(){
   var axiosConfig = {
     headers: {
       'Content-Type': 'application/json',
-      'accept': '*/*',
+      //'accept': '*/*',
+      //'Access-Control-Allow-Origin': '*',
     }
   };
   const ax = require('axios');
 
-  ax.post('http://localhost:5000/suggest', returnJson, axiosConfig).then(resp => {
+  ax.post('http://127.0.0.1:5000/suggest', returnJson, axiosConfig).then(resp => {
     console.log(resp.data['places']);
     latestData = resp.data['places']
-
-
     getNextRoute()
   }).catch(error => {
     console.log(error);
@@ -133,9 +135,6 @@ function getNextRoute(){
     });
   }
 
-var startMarker;
-var destMarker;
-var prevPolyline = []
 
 function drawPolyline(jsonData){
     if (startMarker) {
@@ -192,6 +191,175 @@ function drawPolyline(jsonData){
             })
       })
   }
+
+
+
+
+  function getUserId() {
+    var userid = document.getElementById("UserId").value;
+    return userid;
+  }
+
+
+  function getUsername() {
+    var username = document.getElementById("Username").value;
+    return username;
+  }
+
+  function getDuration() {
+    var duration = document.getElementById("Duration").value;
+    return duration;
+  }
+
+  function getAge() {
+    var age = document.getElementById("Age").value;
+    return age;
+  }
+
+  function getGender() {
+    var gender = document.getElementById("Gender").value;
+    return gender;
+  }
+
+  function getBudget() {
+    var tmp = document.getElementById("Budget").value;
+    alert(tmp)
+    var budget = 99;
+    switch (tmp) {
+      case '$':
+        budget = 0;
+        break;
+      case '$$':
+        budget = 1;
+        break;
+      case '$$$':
+        budget = 2;
+        break;
+      default:
+        break;
+    }
+    alert(budget);
+    return budget;
+  }
+
+  function getPre() {
+    var preference = ['', '', ''];
+    var inputTag = [document.getElementById("tag1").value, document.getElementById("tag2").value, document.getElementById("tag3").value];
+
+    for (var i = 0; i < 3; i++) {
+      switch (inputTag[i]) {
+        case ('1'):
+          //alert(preference[i])
+          preference[i] = "outdoor";
+          //alert(preference[i])
+          break;
+        case ('2'):
+          preference[i] = "museum";
+          break;
+        case ('3'):
+          preference[i] = "historic";
+          break;
+        case ('4'):
+          preference[i] = "park";
+          break;
+        case ('5'):
+          preference[i] = "lake";
+          break;
+        case ('6'):
+          preference[i] = "food";
+          break;
+        case ('7'):
+          preference[i] = "pubs";
+          break;
+        case ('8'):
+          preference[i] = "play";
+          break;
+        case ('9'):
+          preference[i] = "movie";
+          break;
+        case ('10'):
+          preference[i] = "styling";
+          break;
+        case ('11'):
+          preference[i] = "sttire";
+          break;
+        case ('12'):
+          preference[i] = "shoes";
+          break;
+        default:
+          //preference[i] = "";
+          break;
+
+      }
+      //alert(preference[i])
+    }
+    return preference;
+  }
+
+  function setPrefNew() {
+    var returnJson = new Object();
+    returnJson.user_id = '';
+    returnJson.name = getUsername();
+    returnJson.tags = getPre();
+    returnJson.age = getAge();
+    returnJson.gender = getGender();
+    returnJson.avgDuration = getDuration();
+    returnJson.avgBudget = getBudget();
+    returnJson = JSON.stringify(returnJson);
+    console.log(returnJson);
+    var axiosConfig = {
+      headers: {
+        'Content-Type': 'application/json',
+        'accept': '*/*',
+      }
+    };
+    alert('11');
+    const ax = require('axios');
+    ax.post('http://localhost:5000/suggest', returnJson, axiosConfig).then(resp => {
+      console.log(resp.data['places']);
+      alert(resp.data['user_id'])
+      latestData = resp.data['places'];
+      //getNextRoute()
+      alert(latestData);
+    }).catch(error => {
+      console.log(error);
+    });
+
+  }
+
+  function setPrefOld() {
+    var returnJson = new Object();
+    returnJson.user_id = getUserId();
+    returnJson.name = '';
+    returnJson.tags = getPre();
+    returnJson.age = '';
+    returnJson.gender = '';
+    returnJson.avgDuration = getDuration();
+    returnJson.avgBudget = getBudget();
+    returnJson = JSON.stringify(returnJson);
+    console.log(returnJson);
+    //alert('hdgy');
+    // var axiosConfig = {
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'accept': '*/*',
+    //   }
+    // };
+    // ax.post('url to service', returnJson, axiosConfig).then(resp => {
+    //   console.log(resp);
+    // }).catch(error => {
+    //   console.log(error);
+    // });
+
+  }
+
+
+
+
+
+
+
+
 
 function test(){
     var url = "http://localhost:5000/1"
